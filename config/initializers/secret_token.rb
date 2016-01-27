7bb33bca942b09f3bdf3d439065d4eae71221fb0
Dai Le <daile@Dais-MacBook-Pro.local>
2016-01-27 11:07:24 +0900

    new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestUglifier::Application.config.secret_token = '8b6629916ecf7a3f7c4da1129920c26dc1d3d863601636301403713bea57f0c6f55e7b6b34cd2ff763c94cfb6a761ab25fbc754ab8c95aa1d70365e4c12502fb'
